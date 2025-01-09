c0a89c33901a3d65de50979abb8f9af394aa7932
ihbrazil <ihbrazil@users.noreply.github.com>
2025-01-08 21:15:30 -0300

    Publicaçãp
    Atualizações para publicar App

--- telas/TelaInicio/index.js
+++ telas/TelaInicio/index.js
@@ -35,52 +35,65 @@ const TelaInicio = (props) => {
     <ImageBackground source={fundo} style={estilo.imagemFundo}>
       <View style={estilo.tela}>
         <View style={estilo.boxPergunta}>
+          
           <Text style={estilo.titulo}>
             Duvido você acertar!
           </Text>
 
           <View style={estilo.boxQuestao}>
-            <Text style={estilo.texto}>            
-              { primeiroNumero }
-            </Text>
+              <Text style={estilo.texto}>            
+                { primeiroNumero }
+              </Text>
 
-            <Text style={estilo.texto}>
-              X
-            </Text>
+              <Text style={estilo.texto}>
+                X
+              </Text>
 
-            <Text style={estilo.texto}>            
-              { segundoNumero }
-            </Text>
+              <Text style={estilo.texto}>            
+                { segundoNumero }
+              </Text>
 
-            <Text style={estilo.texto}>
-              =
-            </Text>
+              <Text style={estilo.texto}>
+                =
+              </Text>
 
-            <TextInput 
-              textAlign="center"
-              onChangeText={ setRespostaUsuario } 
-              keyboardType="numeric" 
-              value={respostaUsuario.toString()} 
-              autoFocus={true}
-              maxLength={3} 
-              style = { estilo.input }/>
+              <TextInput 
+                textAlign="center"
+                onChangeText={ setRespostaUsuario } 
+                keyboardType="numeric" 
+                value={respostaUsuario.toString()} 
+                autoFocus={true}
+                maxLength={3} 
+                style = { estilo.input }/>
           </View>
 
           <View style={estilo.opcoes}>
+            
             <View style={estilo.boxBotao}>
-              <Button title="Pular" onPress={criarQuestao} color="#e53b62"/>
+              <Button
+                title="Pular"
+                onPress={criarQuestao}
+                color="#e53b62"/>
             </View>
 
             <View style={estilo.boxBotao}>
-              <Button title="Responder" onPress={responder} color="#a0df52"/>
+              <Button
+                title="Responder"
+                onPress={responder}
+                color="#a0df52"/>
             </View>
+
           </View>
 
         </View>
 
         <View style={estilo.boxBotaoTabuada}>
-          <Button title="Ver tabuada" onPress={abrirTelaTabuada} color="#1f4f66"/>
+          <Button
+            title="Ver tabuada"
+            onPress={abrirTelaTabuada}
+            color="#1f4f66"/>
         </View>
+      
       </View>
     </ImageBackground>
   );
